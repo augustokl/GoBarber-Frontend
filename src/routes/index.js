daac1f7372142363244f045541bd5ab55faa44ee
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-02-05 23:14:34 -0300

    Configure private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -7,14 +8,14 @@ import SignUp from '../pages/SignUp';
 import Profile from '../pages/Profile';
 import Dashboard from '../pages/Dashboard';
 
-export function Routes() {
+export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/profile" component={Profile} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
