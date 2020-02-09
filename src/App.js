b7eedd50827b81a2467cb4871b897c767191ff86
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-02-08 22:55:04 -0300

    Add redux-saga, create store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 import GlobalSytle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobalSytle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalSytle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
