b5f905c66d10f84996dafca748652abe65d69ed9
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-02-09 22:21:34 -0300

    refatoring code, and add toast in errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
@@ -18,6 +19,7 @@ function App() {
         <Router history={history}>
           <GlobalSytle />
           <Routes />
+          <ToastContainer />
         </Router>
       </PersistGate>
     </Provider>
