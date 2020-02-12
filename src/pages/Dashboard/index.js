a8f85374a2e4dbf7a3a1ca003c8a5e0f64f65fe2
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-02-11 23:15:00 -0300

    profile add

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import api from '~/services/api';
+// import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>DashBoard</h1>;
+  return <div />;
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <Input
+          name="cofirmPassword"
+          type="password"
+          placeholder="Confirme sua senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Logoff</button>
+    </Container>
+  );
 }
