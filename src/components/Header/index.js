b84b619a88123c730af229141252d18d211842b2
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-02-12 22:53:07 -0300

    fix header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { Container, Content, Profile } from './styles';
@@ -8,6 +9,10 @@ import Notification from '../Notifications';
 import logo from '~/assets/logo-purple.svg';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
+  console.tron.log(profile);
+
   return (
     <Container>
       <Content>
@@ -20,12 +25,15 @@ export default function Header() {
           <Notification />
           <Profile>
             <div>
-              <strong>Augusto Kestering</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Augusto"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
